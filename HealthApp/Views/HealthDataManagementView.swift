d7ec963b0e3e118a937e619f12d703373983d675
ice051128 <49018832+ice051128@users.noreply.github.com>
2023-11-20 20:21:37 +0800

    Update HealthDataManagementView.swift

--- HealthApp/Views/HealthDataManagementView.swift
+++ HealthApp/Views/HealthDataManagementView.swift
@@ -2,7 +2,7 @@
 //  HealthDataManagementView.swift
 //  HealthApp
 //
-//  Created by Eric Li on 14/11/2023.
+//  Created by on 14/11/2023.
 //
 
 import SwiftUI
@@ -14,11 +14,63 @@ struct HealthDataManagementView: View {
                 .font(.title2)
                 .bold()
             Divider()
+//            NavigationView {
+                List {
+                    ForEach(records.indices, id: \.self) { index in
+                        Button(action: {
+                            selectedItem = records[index]
+                        }, label: {
+                            if (!records[index].isDeleted) {
+                                FoodSelectionRow(record: records[index])
+                                    .foregroundColor(.black)
+                            }
+                        })
+                        .frame(maxWidth: .infinity)
+                        .padding(.horizontal, -10)
+                        .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                        .swipeActions(edge: .leading) {	
+                            Button {
+                                finishRecord(record: records[index])
+                            } label: {
+                                Text("Done")
+                                    .bold()
+                            }
+                            .tint(.green)
+                        }
+                    }
+                    .onDelete(perform: deleteItems)
+                    .frame(alignment: .leading)
+                    Button(action: {
+                        showActivitySheet = true
+                    }, label: {
+                        Text("+")
+                    })
+                }
+                .listStyle(.plain)
+                .sheet(item: $selectedItem, content: { item in
+                    FoodSelectionDetailView(record: item)
+                        .environmentObject(modelData)
+//                        .presentationDetents([.large, .medium, .fraction(0.55)])
+                })
+                .sheet(isPresented: $showActivitySheet, content: {
+                    FoodSelectionDetailView(record: nil)
+                        .environmentObject(modelData)
+                })
+//            }
+
+
+            
+             
             Spacer()
+            CalendarView(dayStore: dayStore)
+                .onChange(of: dayStore.currentDate) {
+                    records.nsPredicate = makeDatePredicate(currentDate: dayStore.currentDate)
+                }
+            
         }
     }
-}
+
 
 #Preview {
-    HealthDataManagementView()
+    HealthDataManagementView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
 }
